a9bb5dfa695c79729973e9549513d8c449d4309c
archeltaneka <archeltaneka@ymail.com>
2021-02-13 11:55:30 +0700

    feat: added heatmap results feature in the analyze script

--- analyze.py
+++ analyze.py
@@ -16,10 +16,11 @@ from lib import utils
 from datetime import datetime
 import argparse
 
+print('Initiating the analyzer...')
 arg_parse = argparse.ArgumentParser()
 arg_parse.add_argument('--weight', required=True, help='Path to the weights of pretrained YOLO model')
 arg_parse.add_argument('--video_input', required=True, help='Video to be analyzed')
-# arg_parse.add_argument('--court', required=True, help='Court image to calculate homography transform')
+arg_parse.add_argument('--court', required=True, help='Court image to calculate homography transform')
 arg_parse.add_argument('--player1', required=True, help='Player 1 name')
 arg_parse.add_argument('--player2', required=True, help='Player 2 name')
 arg_parse.add_argument('--video_output', required=True, help='Output video save path')
@@ -28,7 +29,9 @@ args = vars(arg_parse.parse_args())
 
 # initiate YOLOV3 model
 model = yolo3_custom.make_yolov3_model()
+print('[INFO] YOLOV3 weights found, loading weights...')
 weight_reader = yolo3_custom.WeightReader(args['weight'])
+print('[STATUS] Weights have been loaded successfully')
 weight_reader.load_weights(model)
 
 net_h, net_w = 416, 416 # input width & height
@@ -81,43 +84,58 @@ bar = progressbar.ProgressBar(maxval=n_frames, widgets=[progressbar.Bar('=', '['
 bar.start()
 start_time = datetime.now()
 
+print('[INFO] Reading input video...')
 coordinates = []
-while grabbed:
-    frame_number += 1
-    grabbed, frame = cap.read()
+# while grabbed:
+#     frame_number += 1
+#     grabbed, frame = cap.read()
         
-    if grabbed:
-        frame = cv2.resize(frame, (video_size[0], video_size[1]), interpolation=cv2.INTER_AREA)
+#     if grabbed:
+#         frame = cv2.resize(frame, (video_size[0], video_size[1]), interpolation=cv2.INTER_AREA)
 
-        # players' positions
-        frame_h, frame_w, _ = frame.shape
-        new_frame = yolo3_custom.preprocess_input(frame, net_h, net_w)
+#         # players' positions
+#         frame_h, frame_w, _ = frame.shape
+#         new_frame = yolo3_custom.preprocess_input(frame, net_h, net_w)
         
-        preds = model.predict(new_frame)
-        bboxes = []
-        for i in range(len(preds)):
-            bboxes += yolo3_custom.decode_netout(preds[i][0], anchors[i], obj_thresh, nms_thresh, net_h, net_w)
+#         preds = model.predict(new_frame)
+#         bboxes = []
+#         for i in range(len(preds)):
+#             bboxes += yolo3_custom.decode_netout(preds[i][0], anchors[i], obj_thresh, nms_thresh, net_h, net_w)
         
-        yolo3_custom.correct_yolo_boxes(bboxes, frame_h, frame_w, net_h, net_w)
-        yolo3_custom.do_nms(bboxes, nms_thresh)
-        frame = yolo3_custom.draw_boxes(frame, frame_number, bboxes, labels, obj_thresh, src_pts, player_info, player=player_info['player_A']['label'])
+#         yolo3_custom.correct_yolo_boxes(bboxes, frame_h, frame_w, net_h, net_w)
+#         yolo3_custom.do_nms(bboxes, nms_thresh)
+#         frame = yolo3_custom.draw_boxes(frame, frame_number, bboxes, labels, obj_thresh, src_pts, player_info, player=player_info['player_A']['label'])
 
-        # write each frame in a new video
-#         writer.write(frame)
+#         # write each frame in a new video
+# #         writer.write(frame)
         
-        cv2.imshow('Final Result', frame)
-        if cv2.waitKey(1) & 0XFF == ord('q'):
-            break
+#         cv2.imshow('Final Result', frame)
+#         if cv2.waitKey(1) & 0XFF == ord('q'):
+#             break
     
-        bar.update(frame_number) # updates the progress bar
+#         bar.update(frame_number) # updates the progress bar
     
-    else:
-        grabbed = False
+#     else:
+#         grabbed = False
 
-print("Finished")
-print("Elapsed time:", datetime.now() - start_time)
-writer.release()
-cap.release()
-cv2.destroyAllWindows()
+print("[STATUS] Analyzing input video finished")
+print("[INFO] Elapsed time:", datetime.now() - start_time)
+# writer.release()
+print('[INFO] Saving output video as:', args['video_output'])
+# cap.release()
+# cv2.destroyAllWindows()
+print('[STATUS] Output video has been successfully saved')
 
-pickle.dump(player_info, open(args['player_info'], 'wb'))
+print('[INFO] Saving player info as:', args['player_info'])
+# pickle.dump(player_info, open(args['player_info'], 'wb'))
+print('[STATUS] Player info has been successfully saved')
+
+print('[INFO] Loading player info:', args['player_info'])
+player_info = pickle.load(open(args['player_info'], 'rb'))
+court_path = args['court']
+
+print('[STATUS] Showing results for both players...')
+print('[INFO] Number of coordinates for the 1st player:', len(player_info['player_A']['player_coords']))
+utils.generate_heatmap(court_path, player_info['player_A']['player_coords'], player_info['player_A']['label'], n_frames/video_fps, bins=15)
+print('[INFO] Number of coordinates for the 2nd player:', len(player_info['player_B']['player_coords']))
+utils.generate_heatmap(court_path, player_info['player_B']['player_coords'], player_info['player_B']['label'], n_frames/video_fps, bins=15)
--- lib/utils.py
+++ lib/utils.py
@@ -1,4 +1,6 @@
+import numpy as np
 import cv2
+import matplotlib.pyplot as plt
 
 def init_court_capture(file_path, scaling_factor):
     
@@ -10,3 +12,99 @@ def init_court_capture(file_path, scaling_factor):
     img = cv2.resize(frame, (width, length), interpolation=cv2.INTER_AREA)
     
     return img
+
+def drawPlayers(im, pred_boxes, pred_classes, showResult=False):
+    
+    # box config
+    color = [255, 0, 0]
+    thickness = 3
+    radius = 3
+
+    i  = 0
+    for box in pred_boxes:
+
+        # get box's x & y positions
+        x1 = int(box.xmin)
+        y1 = int(box.ymin)
+        x2 = int(box.xmax)
+        y2 = int(box.ymax)
+
+        xc = x1 + int((x2 - x1)/2) # center point
+        player_pos = (xc - 1, y2 - 25) # player's position
+
+        court = Polygon(src_pts)
+
+        # Draw only players that are within the court
+        if (box.classes[0] > obj_thresh) & (Point(player_pos).within(court)):
+            if showResult:
+                print("[% 3d, % 3d]" %(xc, y2))
+
+            cv2.circle(im, player_pos, radius, color, thickness)
+            i = i + 1            
+
+    if showResult:
+        plt.imshow(im)
+#         cv2.imshow('Court', im)
+        
+#         cv2.waitKey(0)
+#         cv2.destroyAllWindows()
+
+def homographyTransform(img_src, img_dst, showResult=False):
+
+    # Calculate Homography
+    h, status = cv2.findHomography(src_pts, dst_pts)
+    img_out = cv2.warpPerspective(img_src, h, (img_dst.shape[1], img_dst.shape[0]))
+    
+    if showResult:
+        plt.imshow(img_out)
+#         cv2.imshow('Court', img_out)
+        
+#         cv2.waitKey(0)
+#         cv2.destroyAllWindows()
+
+    return img_out
+
+def getPlayersMask(im):
+    lower_range = np.array([255,0,0])                         # Set the Lower range value of blue in BGR
+    upper_range = np.array([255,155,155])                     # Set the Upper range value of blue in BGR
+    mask = cv2.inRange(im, lower_range, upper_range)          # Create a mask with range
+    result = cv2.bitwise_and(im, im, mask = mask)             # Performing bitwise and operation with mask in img variable
+    # cv2_imshow(result)                              
+
+    return cv2.inRange(result, lower_range, upper_range) 
+
+def drawPlayersOnCourt(im, coord, color, radius=10):
+    # draw a circle on players' positions according to their coordinates
+    for player_pos in coord:
+        center = (player_pos[0], player_pos[1])
+        cv2.circle(im, center, radius, color, thickness=-1)
+        
+    return im
+
+def generate_heatmap(court_path, coords, player, video_duration, bins=25):
+    
+    pixel_to_meter = 0.0002645833
+    diff = [(x1[0]-x0[0], x1[1]-x0[1]) for x0,x1 in zip(coords[0::], coords[1::])]
+    dist = [np.sqrt(x**2 + y**2) for x,y in diff]
+    travel_distance = int(np.sum(dist) * pixel_to_meter)
+    speed = np.round((travel_distance/video_duration) * 3600/1000, 1)
+    
+    court = cv2.imread(court_path)
+    
+    pos_x = [pos[0] for pos in coords]
+    pos_y = [pos[1] for pos in coords]
+    hm, edge_x, edge_y = np.histogram2d(pos_x, pos_y, bins=bins, normed=True)
+    extent = [0, court.shape[1], court.shape[0], 1]
+    
+    fig, ax = plt.subplots(1, 1, figsize=(10,10))
+    ax.set_xlim(0, court.shape[1])
+    ax.set_ylim(court.shape[0], 0)
+    
+    fig1 = ax.imshow(court[:,:,[2,1,0]])
+    fig2 = ax.imshow(hm.T, cmap='hot_r', alpha=0.8, interpolation='gaussian', extent=extent)
+    
+    plt.axis('off')
+    plt.title('Heatmap: {} (1st Set)'.format(player))
+    plt.annotate('Court Coverage: {}m\n'.format(travel_distance), (.19,.1), xycoords='figure fraction')
+    plt.annotate('Speed: {}km/h'.format(speed), (.19,.1), xycoords='figure fraction')
+    plt.show()
